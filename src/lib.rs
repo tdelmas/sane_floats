ef8e940a018fc829fba44b8d0cc6753dddeb8c32
Tom <1955774+tdelmas@users.noreply.github.com>
2025-03-25 21:09:59 +0000

    draft
    Signed-off-by: GitHub <noreply@github.com>

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,26 @@ pub trait Sane {
 }
 
 impl Sane for f64 {
-    type SqrtOutput = f64;
-
+    type SqrtOutput = Self;
     
     fn sqrt(&self) -> Self::SqrtOutput {
-        self.sqrt()
+        if self.is_sign_negative() {
+            Self::NAN
+        } else {
+            (*self).sqrt()
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_sqrt() {
+        #[allow(clippy::float_cmp)]
+        assert_eq!(4.0.sqrt(), 2.0);
+
+        assert!((-0.0).sqrt().is_nan());
     }
 }
